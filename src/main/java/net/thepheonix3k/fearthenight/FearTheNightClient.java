bad6ac3d62eaf42a20767f12e3a6b12b894e731b
ThePheonix3k <teddy.werne@gmail.com>
2024-06-27 14:12:02 -0400

    Intial Commit

---
+++ src/main/java/net/thepheonix3k/fearthenight/FearTheNightClient.java
@@ -0,0 +1,10 @@
+package net.thepheonix3k.fearthenight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FearTheNightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/thepheonix3k/fearthenight/FearTheNightDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thepheonix3k.fearthenight;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FearTheNightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
